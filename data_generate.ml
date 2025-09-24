ab90e42290b62dcca6ee014d595b8963b32f8c4b
Hongbo Zhang <bobzhang1988@gmail.com>
2025-09-24 11:00:07 +0800

    Add decoder implementation and update related tests and types

---
+++ data_generate.ml
@@ -0,0 +1,14 @@
+
+let f data = 
+    Marshal.to_string data []
+
+;;
+
+let print_to_mbt_fmt data =
+   let s =  ref "[" in 
+  f data |>
+  String.iter  (fun x -> s := !s ^  Printf.sprintf "b'\\x%02x'," (Char.code x));
+  !s ^ "]" |>print_endline
+
+
+;; print_to_mbt_fmt(123)  
